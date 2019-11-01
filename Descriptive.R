9b93d4df3936a93ac9f178659541ca1b7be624f4
Shelley Facente <42495531+sfacente@users.noreply.github.com>
2019-10-31 18:59:41 -0700

    Update Descriptive.R

--- Descriptive.R
+++ Descriptive.R
@@ -1,11 +1,12 @@
 #set working directory
-setwd("~/Desktop/lizzyandshelley/Dataset") #LIZZY
+#setwd("~/Desktop/lizzyandshelley/Dataset") #LIZZY
 setwd("~/GitHub/lizzyandshelley/Dataset")  #SHELLEY
 
 #open packages
 library(here)
 library(foreign)
 library(labelled)
+library(dplyr)
 
 #load the data
 df <- read.spss("Ricky.SAV", use.value.labels = TRUE, to.data.frame=TRUE)
@@ -27,3 +28,11 @@ table(df$GENDER, useNA = "ifany")
 table(df$HOMELESS, useNA = "ifany")
 table(df$FU1, useNA = "ifany")
 table(df$FU2, useNA = "ifany")
+
+#select variables
+dfs <- df %>%
+  select("GENDER", "AGE", "RACELG", "HOMELESS", "HOMELESS1", "HOMELESS2", "MOSHOME", "MOSHOME1", "MOSHOM2", "NVHOUSED", 
+         "ALWYSHL", "HOUSED", "HLP6M", "HLP6M1", "HLP6M2", "NHLP6M", "NHLP6M1", "NHLP6M2", "NDLSRCF", "NDLSRCF1", 
+         "NDLSRCF2", "PUBINJ", "PUBINJ1", "PUBINJ2", "IJWOTH", "IJWOTH1", "IJWOTH2", "IJNON", "IJNON1", "IJNON2", 
+         "ODP6M", "ODP6M1", "ODP6M2", "ODTIMES1", "ODTIMES2", "WOD6M", "WOD6M1", "WOD6M2", "ODTRAIN1", "NALOWN1", 
+         "NALAVAIL1", "POLICE1", "POLICE2", "POLTIME1", "ARREST1", "ARREST2", "JAIL1", "JAIL2", "TX6MOS1", "TX6MOS2")
