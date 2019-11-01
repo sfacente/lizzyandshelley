0e0175b3d6ad5d6350f488bc2d180355b24a01af
ekinnard <ekinnard@berkeley.edu>
2019-10-31 19:00:01 -0700

    Update Descriptive.R

--- Descriptive.R
+++ Descriptive.R
@@ -18,12 +18,29 @@ df.labels <- cbind(QDSlabels, df.labels)
 names(df.labels) <- c("VAR_ID", "description")
 rm(QDSlabels)
 
+#USE THIS CODE TO LOOK UP A VARIABLE DESCRIPTION
+#df.labels$description[df.labels$VAR_ID=="GENDER"]
+
+
 #inspect the data
 names(df)
 dim(df)
 length(unique(df$SUBJECT))
 
 table(df$GENDER, useNA = "ifany")
-table(df$HOMELESS, useNA = "ifany")
 table(df$FU1, useNA = "ifany")
 table(df$FU2, useNA = "ifany")
+
+#look at homeless variables
+table(df$HOMELESS, useNA = "ifany")
+table(df$HOMELESS1, useNA = "ifany")
+table(df$HOMELESS2, useNA = "ifany")
+
+#look at homeless variables ricky created
+table(df$NVHOUSED, useNA = "ifany")
+table(df$ALWYSHL, useNA = "ifany")
+table(df$HOUSED, useNA = "ifany")
+
+#look at initiation variables
+
+hlp6m
